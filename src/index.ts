9a710dca7c51b5b46a48c327f4088c4bd8848ae5
rosostolato <eduardo.rosostolato@gmail.com>
2019-09-04 13:00:12 -0300

    sorting export order

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 
 export { RestClient } from './rest-client';
 
+export { Map } from './decorators/map';
 export { Client } from './decorators/client';
 export { Headers } from './decorators/headers';
-export { Map } from './decorators/map';
-export { Timeout } from './decorators/timeout';
 export { OnEmit } from './decorators/on-emit';
-export { Body, Header, Query, PlainQuery, Path, PlainBody } from './decorators/parameters';
+export { Timeout } from './decorators/timeout';
 export { MediaType, Produces } from './decorators/produces';
 export { Get, Post, Patch, Put, Delete, Head } from './decorators/request-methods';
+export { Body, Header, Query, PlainQuery, Path, PlainBody } from './decorators/parameters';
